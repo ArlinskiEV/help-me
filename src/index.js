aab51834ff1fcf9a7bd6dbe061a914a553a1b896
ArlinskiEV <ArlinskiEV@gmail.com>
2018-03-11 03:20:51 +0300

    help-me/fix: implement brute-force

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ module.exports = function count(s, pairs) {
   // your implementation
   let answer = 0;
   // pairs = [[prime, pow],...];
-  let mul = [...pairs].sort((a,b) => a[0] - b[0]);
-
+  // let mul = [...pairs].sort((a,b) => a[0] - b[0]);
+  let mul = [...pairs];
   //-----------------------------
   // advanced zero ?
   //-----------------------------
@@ -12,17 +12,31 @@ module.exports = function count(s, pairs) {
   function testK(k) {
     let memory = {};
     let commonDivisor = (num) => {
-      if (!memory.num) {
-        memory.num = mul.some((item) => (num % item[0] === 0));
-      }
-      return memory.num;
+      // if (memory.num === undefined) {
+      //   memory.num = mul.some((item) => (num % item[0] === 0));
+      // }
+      // return memory.num;
+      return mul.some((item) => (num % item[0] === 0));
     };
-    return Array.prototype.every.call(s, (item, index) => (commonDivisor(k + index) == (item === '0')));
+    // true, item === '0' / false, item === '1'
+    return Array.prototype.every.call(s, (item, index) => (commonDivisor(k + index) === (item === '0')));
+
+    // let fromArr = Array.prototype.every.call(s, (item, index) => {
+    //   let common = commonDivisor(k + index);
+
+    //   let res = item === '0' ? common : !common;
+    //   console.log(`k+index=${k + index},item=${item},common=${common}`);
+    //   return res;
+    // });
+    // console.log(`k=${k} fromArr=${fromArr}`);
+    // return fromArr;
   };
 
   let n = mul.reduce((prev, item) => prev * Math.pow(item[0], item[1]), 1);
   console.log('n=' + n);
-  for (let i = 0; i <= n; i++) {
+  //stopper
+  n = n>6000000 ? 6000000 : n;
+  for (let i = 1; i <= n; i++) {
     if (testK(i)) {
       answer += 1;
       // console.log(`i=${i}`);
@@ -40,7 +54,7 @@ module.exports = function count(s, pairs) {
 // N = 6
 // NOD(k + 0, N) == 1
 // k = 0/1/2/3/4/5/6
-// nod=-/1/2/3/4/1/6
+// nod=1/1/2/3/4/1/6
 // 1, 5 => 2
 // -------------------
 // count('0', [[3, 1], [2, 1]]);
@@ -57,7 +71,7 @@ module.exports = function count(s, pairs) {
 // NOD(k + 0, N) != 1
 // NOD(k + 1, N) == 1
 // k = 0/1/2/3/4/5/6/7/8/9-/10/11/12/13/14/15/16/17/18/19/20/21/22/23/24/25/26/27
-// nod=-/1/1/3/1/1/3/1/1/9-/1-/1-/3-/1-/1-/3-/1-/1-/9-/1-/1-/3-/1-/1-/3-/1-/1-/27
+// nod=1/1/1/3/1/1/3/1/1/9-/1-/1-/3-/1-/1-/3-/1-/1-/9-/1-/1-/3-/1-/1-/3-/1-/1-/27
 // k+1=1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21/22/23/24/25/26/27/28
 // nod=1/1/3/1/1/3/1/1/9/1-/1-/3-/1-/1-/3-/1-/1-/9-/1-/1-/3-/1-/1-/3-/1-/1-/27/1-
 // 3,6,9,12,15,18,21,24,27 => 9
