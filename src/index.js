a59ceb6e240a70035098fd514ab1fe1808d4e8e1
ArlinskiEV <ArlinskiEV@gmail.com>
2018-03-17 10:38:27 +0300

    help-me/features: test

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ module.exports = function count(s, pairs) {
   // advanced zero ?
   // '11' in s + [2,x] in pairs => 0
   //-----------------------------
+  // *how many* such integer numbers `k` exist (`0 <= k <= N`),
+  // which follow next condition: 
+  // greatest common divisor of (`k + j` and `N`) is 1 if `s[j] === 1` *AND* 
+  // greatest common divisor of (`k + j` and `N`) is *NOT* 1 if `s[j] === 0`.
+
+
 
   // evil time
   function testK(k) {
@@ -18,7 +24,7 @@ module.exports = function count(s, pairs) {
   };
 
   let n = mul.reduce((prev, item) => prev * Math.pow(item[0], item[1]), 1);
-  // console.log('n=' + n);
+  console.log('n=' + n);
   //stopper
   if (n > 5800000) return 0;
   //WHY??? form 0 => 0 is incorrect!! WTF??
@@ -29,7 +35,7 @@ module.exports = function count(s, pairs) {
     }
   };
 
-  // console.log('answer=' + answer);
+  console.log('answer=' + answer);
   const result = answer % 1000000007;
   return result;
 }
